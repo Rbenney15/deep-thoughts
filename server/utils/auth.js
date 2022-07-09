f31610c5c06e06b02afd5b8ccd4365376c5d30fa
Rob Benney <rob.benney@gmail.com>
2022-07-09 16:42:54 -0400

    corrected syntax issue in server file

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -12,6 +12,8 @@ module.exports = {
   authMiddleware: function({ req }) {
     // allows token to be sent via req.body, req.query, or headers
     let token = req.body.token || req.query.token || req.headers.authorization;
+
+      console.log(token)
   
     // separate "Bearer" from "<tokenvalue>"
     if (req.headers.authorization) {
